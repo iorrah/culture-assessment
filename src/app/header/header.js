ed102612849dfe36ba870bd77fbe64063303b495
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-01-17 15:16:58 +0100

    Undo change made for testing purpose

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -43,7 +43,7 @@ class Header extends React.Component {
       <header>
         <div className="wrapper">
           <button className="logo">
-            Culture Assessment App
+            Culture Assessment
           </button>
 
           {canRenderRetakeButton ? (
